1056d259619691d3e0ccb9616a8c7e28ddcc83d9
NA-Asraful-Khan <soyonshikhor@gmail.com>
2022-04-19 21:26:53 +0600

    Google Sign In done

--- src/Componantes/LogIn/Login/Login.js
+++ src/Componantes/LogIn/Login/Login.js
@@ -1,17 +1,27 @@
 import { Button } from 'bootstrap';
-import React from 'react';
+import React, { useState } from 'react';
 import { Form } from 'react-bootstrap';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+import SocialLogin from '../../Shared/SocialLogin/SocialLogin';
 
 const Login = () => {
+    let errorElement;
     const [
         signInWithEmailAndPassword,
         user,
         loading,
         error,
     ] = useSignInWithEmailAndPassword(auth);
+
+    const [email, setEmail] = useState('');
+    const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(
+        auth
+    );
 
     const navigate = useNavigate();
     const location = useLocation();
@@ -21,34 +31,64 @@ const Login = () => {
         navigate('/login');
     }
 
-    const handleSignin = event =>{
+    const handleSignin = event => {
         event.preventDefault();
         const email = event.target.email.value;
         const password = event.target.password.value;
 
         signInWithEmailAndPassword(email, password);
-        
+
     }
+    const resetPassword = async (event) => {
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        if (!email) {
+            return alert("Please input email");
+        } else {
+            await sendPasswordResetEmail(email);
+            console.log('email sent');
+        }
+
+
+    };
 
     if (user) {
         navigate(from);
-      }
+    }
+
+    if (error) {
+        errorElement = <p className='text-danger'>Error: {error?.message}</p>
+    }
     return (
         <div className='container my-5 py-5'>
             <h2>Log In</h2>
             <Form onSubmit={handleSignin} className='w-50 mx-auto text-start my-5'>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control name='email' type="email" placeholder="Enter email" />
+                    <Form.Control value={email}
+                        onChange={(e) => setEmail(e.target.value)} name='email' type="email" placeholder="Enter email" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control name='password' type="password" placeholder="Password" />
+                    <Form.Control name='password' type="password" placeholder="Password" required />
                 </Form.Group>
+                {errorElement};
                 <button type='submit' className="btn btn-primary">Login</button>
-                <p className="my-2">Want to Create New Account? <Link className="text-decoration-none text-primary" to="/signup">Click for Sign Up</Link></p>
             </Form>
+            <div className='container w-50 mx-auto'>
+                <p className='text-start'>Want to Create New Account? <Link className="text-decoration-none text-primary " to="/signup">Click for Sign Up</Link></p>
+                <p className=" text-start">Want to Create New Account? <button className="text-decoration-none text-primary btn" onClick={async () => {
+                    if (!email) {
+                        return toast("Please Input email");
+                    } else {
+                        await sendPasswordResetEmail(email);
+                        toast('Password Reset Email Sent');
+                    }
+                }}>Reset Password</button></p>
+            </div>
+            <SocialLogin></SocialLogin>
+            <ToastContainer />
         </div>
     );
 };
--- src/Componantes/LogIn/SignUp/SignUp.js
+++ src/Componantes/LogIn/SignUp/SignUp.js
@@ -3,57 +3,68 @@ import { Form } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+import SocialLogin from '../../Shared/SocialLogin/SocialLogin';
 
 const SignUp = () => {
-
+    let errorElement;
     const [
         createUserWithEmailAndPassword,
         user,
         loading,
         error,
-      ] = useCreateUserWithEmailAndPassword(auth, {sendEmailVerification: true});
-      const [updateProfile, updating, updateError] = useUpdateProfile(auth);
-      const navigate = useNavigate();
+    ] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+    const navigate = useNavigate();
 
     const navigateLogin = () => {
         navigate('/login');
     }
-    const handleSignup =async (event) =>{
+    const handleSignup = async (event) => {
         event.preventDefault();
         const name = event.target.name.value;
         const email = event.target.email.value;
         const password = event.target.password.value;
-        if(password.length<6){
-            return alert("password should be minimum 6 character");
+        if (password.length < 6) {
+            return toast("Password should be minimum 6 character");
         }
         await createUserWithEmailAndPassword(email, password);
         await updateProfile({ displayName: name });
         navigate('/home');
     }
+
+    if (error) {
+        errorElement = <p className='text-danger'>Error: {error?.message}</p>
+    }
     return (
         <div className='container my-4 py-5'>
             <h2>Sign Up</h2>
             <Form onSubmit={handleSignup} className='w-50 mx-auto text-start'>
-                <Form.Group  className="mb-3" controlId="formBasicName">
+                <Form.Group className="mb-3" controlId="formBasicName">
                     <Form.Label>Your Name</Form.Label>
                     <Form.Control name='name' type="text" placeholder="Your Name" />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control name='email' type="email" placeholder="Enter email" />
+                    <Form.Control name='email' type="email" placeholder="Enter email" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control name='password' type="password" placeholder="Password" />
+                    <Form.Control name='password' type="password" placeholder="Password" required />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Agree With Terms And Condition" />
                 </Form.Group>
+                {errorElement};
                 <button type='submit' className="btn btn-primary">Sign Up</button>
                 <p>Already Have an Account? <Link className="text-decoration-none text-primary" to="/login">Click for Log In</Link></p>
             </Form>
+            <SocialLogin></SocialLogin>
+            <ToastContainer />
         </div>
     );
 };
